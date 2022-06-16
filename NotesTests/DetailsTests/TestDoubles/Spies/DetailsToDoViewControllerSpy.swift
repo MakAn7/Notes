7f9e28af9dfa29e1bdeec09eeeb82ba732ead060
Anton Makarov <makarov070786@mail.ru>
2022-06-16 07:50:39 +0300

    homework9: реализовал Unit Test for DetailsToDoPresenter func presentModel(model: DetailModel.InitForm.Response)

---
+++ NotesTests/DetailsTests/TestDoubles/Spies/DetailsToDoViewControllerSpy.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsToDoViewControllerSpy.swift
+//  NotesTests
+//
+//  Created by Антон Макаров on 16.06.2022.
+//
+
+import Foundation
+@testable import Notes
+
+final class DetailsToDoViewControllerSpy: DetailsToDoDisplayLogic {
+    // MARK: - Private Properties
+    private (set) var didDisplayDataCalled = false
+
+    // MARK: - Public Methods
+    func displayData(model: DetailModel.InitForm.ViewModel) {
+        didDisplayDataCalled = true
+    }
+}
